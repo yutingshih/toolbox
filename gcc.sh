599ff43e36392aac5a561abb20dc6e58f5a39cb0
tings0802 <nick19980922@gmail.com>
2020-08-30 03:50:41 +0800

    feat: generate the modulefiles based on gcc's version automatically

--- gcc.sh
+++ gcc.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 VER=${1:-6.3.0}
+URL=https://ftp.gnu.org/gnu/gcc/gcc-${VER}/gcc-${VER}.tar.gz
 TOPDIR=${2:-${HOME}}
 SRCDIR=${TOPDIR}/gcc-${VER}
 BLDDIR=${TOPDIR}/gcc-${VER}/build
 APPDIR=${3:-${SRCDIR}}
-URL=https://mirror.ossplanet.net/gnu/gcc/gcc-${VER}/gcc-${VER}.tar.gz
+MODULE=modules/gcc/${VER}
 
-if [[ ! -d ${SRCDIR} ]]; then
+if test ! -f ${MODULE}; then 
+    cp modules/gcc/sample ${MODULE}
+    sed -i "s~\${APPDIR}~${APPDIR}~g" ${MODULE}
+fi
+
+if [ ! -d ${SRCDIR} ]; then
     wget ${URL} -O - | tar -xzC ${TOPDIR}
 fi
 
@@ -16,7 +22,7 @@ unset LIBRARY_PATH CPATH C_INCLUDE_PATH PKG_CONFIG_PATH CPLUS_INCLUDE_PATH INCLU
 cd ${SRCDIR} && ${SRCDIR}/contrib/download_prerequisites
 
 mkdir -p ${BLDDIR} && cd ${BLDDIR}
-${SRCDIR}/configure --prefix=${APPDIR} --enable-languages=c,c++ --disable-multilib
+${SRCDIR}/configure --prefix=${APPDIR} --enable-languages=c,c++,gfortran --disable-multilib
 make -j $(nproc)
 make install
 
