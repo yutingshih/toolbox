62acb9ea51a54bfb376fc67ad7409e76968a004d
tings0802 <nick19980922@gmail.com>
2020-08-30 12:31:47 +0800

    fix: correct a typo in the configure options

--- gcc.sh
+++ gcc.sh
@@ -8,21 +8,22 @@ BLDDIR=${TOPDIR}/gcc-${VER}/build
 APPDIR=${3:-${SRCDIR}}
 MODULE=modules/gcc/${VER}
 
-if test ! -f ${MODULE}; then 
+if [ ! -f ${MODULE} ]; then 
     cp modules/gcc/sample ${MODULE}
     sed -i "s~\${APPDIR}~${APPDIR}~g" ${MODULE}
+    echo "Modulefile created"
+else
+    echo "Modulefile already exists"
 fi
 
-if [ ! -d ${SRCDIR} ]; then
-    wget ${URL} -O - | tar -xzC ${TOPDIR}
-fi
+test ! -d ${SRCDIR} && wget ${URL} -O - | tar -xzC ${TOPDIR}
 
 unset LIBRARY_PATH CPATH C_INCLUDE_PATH PKG_CONFIG_PATH CPLUS_INCLUDE_PATH INCLUDE
 
 cd ${SRCDIR} && ${SRCDIR}/contrib/download_prerequisites
 
 mkdir -p ${BLDDIR} && cd ${BLDDIR}
-${SRCDIR}/configure --prefix=${APPDIR} --enable-languages=c,c++,gfortran --disable-multilib
+${SRCDIR}/configure --prefix=${APPDIR} --enable-languages=c,c++,fortran --disable-multilib
 make -j $(nproc)
 make install
 
